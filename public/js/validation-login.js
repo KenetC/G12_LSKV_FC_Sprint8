d184aedd37a171a1b38cd3115b839932c2127fc2
sharonmfg03 <sharonmelanie.flores@gmail.com>
2022-04-02 08:58:26 -0300

    Sharon- agredando productos

--- public/js/validation-login.js
+++ public/js/validation-login.js
@@ -13,6 +13,14 @@ window.addEventListener("load", function(){
            emailError.innerHTML = "<p>" + error_email + "</p>";
            log('Entre en email')
        }
+
+      // emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+   // if (emailRegex.test(email.value)) {
+      
+    //} else {
+      //emailError.innerText = "Ingrese un correo valido";
+    //}
    });
 
    let passError = document.querySelector('div.text-danger-password');
@@ -20,9 +28,9 @@ window.addEventListener("load", function(){
    let err = undefined;
    password.addEventListener("blur", function(e){ 
       if(password.value == ""){ 
-         err = "Debe escribir una contraseña";
+         err = "Debes escribir una contraseña";
       }else if(password.value.length < 6){ 
-         err = "Debe escribir almenos 6 caracteres";
+         err = "Debes escribir al menos 6 caracteres";
       }else err ="";
       if(err != undefined) { 
          passError.innerHTML = "<p>" + err + "</p>";
